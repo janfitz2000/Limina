eead346cd35bc87cac6a9e4545694b8c3b64ad38
Jan FitzGibbon <janfitzgibbon@192.168.1.114>
2025-06-22 19:02:17 +0100

    Remove WooCommerce and uploads from repo and update .gitignore

--- limina-platform-new/wordpress-woocommerce-dev/wp-content/plugins/woocommerce/assets/client/blocks/wc-cart-checkout-base-frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '8ef63f9f37dcac4d2fb0');
--- limina-platform-new/wordpress-woocommerce-dev/wp-content/plugins/woocommerce/assets/client/blocks/wc-types.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '99f253ed9c4c6493f0d5');
